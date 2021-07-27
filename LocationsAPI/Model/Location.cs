43bd73b3ff199603b477292121f4fdcba3347f47
furkan <furkan.meydan@hotmail.com>
2021-07-27 21:50:40 +0300

    Add project files.

---
+++ LocationsAPI/Model/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocationsAPI.Model
+{
+    public class Location
+    {
+        public int id { get; set; }
+        public decimal latitude { get; set; }
+
+        public decimal longitude { get; set; }
+        public string carOwnerID { get; set; }
+
+    }
+}
---
+++ LocationsAPI/Model/LocationDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocationsAPI.Model
+{
+    public class LocationDBContext : DbContext
+    {
+        public DbSet<Location> Locations { get; set; }
+
+        public LocationDBContext(DbContextOptions<LocationDBContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Location>().ToTable("Location");
+        }
+    }
+}
